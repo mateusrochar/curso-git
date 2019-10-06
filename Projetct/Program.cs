0cc2b1f5dd70dedc38b940d595ad083b5eabbd0f
Mateus Rocha <mateusrochar@gmail.com>
2019-10-06 14:43:52 -0300

    Projeto adicionado nova frase Bom dia em alemão

--- Projetct/Program.cs
+++ Projetct/Program.cs
@@ -11,6 +11,7 @@ namespace Projetct
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Word");
+            Console.WriteLine("Gothen Morgan");
         }
     }
 }
